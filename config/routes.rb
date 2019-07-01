5098bdccb269a83383010532fdd271054eb924d5
Tin <tin@Tins-MacBook-Pro.local>
2019-07-02 00:43:30 +0500

    Remove ckeditor and deploy

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  mount Ckeditor::Engine => '/ckeditor'
+  # mount Ckeditor::Engine => '/ckeditor'
   resources :cvs
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
